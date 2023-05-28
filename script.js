04b81efafbc32474dbda7cbe8563197618e6c2d2
Htet Myat Aung <51772284+htetmyataung234@users.noreply.github.com>
2023-05-28 12:32:06 +0630

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  const shoppingList = [
+    'Nrc',
+    'bag',
+    'toothbrush',
+    'toothpaste',
+    'soap',
+    'shampoo',
+    'roll on',
+    'hair spray',
+    'hair gel',
+    'perfume',
+    'sneaker to go out and one slipper to go near',
+    'trousers',
+    'shirts',
+    'raincoat or umbrella',
+    'socks',
+    'charger/earphone/extension cable'
+  ];
+
+  const shoppingListElement = document.getElementById('shopping-list');
+  const saveButton = document.getElementById('save-btn');
+  
+  shoppingList.forEach(item => {
+    const listItem = document.createElement('li');
+
+    const checkboxLabel = document.createElement('label');
+    checkboxLabel.classList.add('checkbox-label');
+    
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.name = 'item';
+    checkbox.value = item;
+
+    checkboxLabel.appendChild(checkbox);
+    checkboxLabel.appendChild(document.createTextNode(item));
+
+    listItem.appendChild(checkboxLabel);
+    shoppingListElement.appendChild(listItem);
+  });
+
+  saveButton.addEventListener('click', () => {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    const selectedItems = Array.from(checkboxes).map(checkbox => checkbox.value);
+
+    // You can customize the save functionality here.
+    // For example, you can save the selected items to a server or local storage.
+    console.log('Selected items:', selectedItems);
+  });
+});
